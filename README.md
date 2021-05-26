6d447b01aed6880c09cacf595867866079709872
Ricardo Pina <naspter@gmail.com>
2021-05-26 13:57:29 +0100

    first release

--- README.md
+++ README.md
@@ -2,11 +2,29 @@
 
 Service in  Node JS to classify images.
 
-To install
+## Required 
 
-npm instal
-
-Windows required
+Windows
 
 Install Visual Studio Community to compile package @tensorflow/tfjs-node and @tensorflow/tfjs-node-gpu
 
+## Installation
+```bash
+npm install
+```
+## Usage
+To classify image with extension JPG, PNG, GIF, BMP
+```
+Endpoint : http://[HOST]:[PORT]/image/?url=[IMAGE_URL]
+```
+To classify GIF Animated frame by frame
+```
+Endpoint : http://[HOST]:[PORT]/imagegif/?url=[IMAGE_URL]
+```
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+Please make sure to update tests as appropriate.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ app.js
@@ -0,0 +1,69 @@
+const express = require('express')
+const axios = require('axios') //you can use any http client
+const tf = require('@tensorflow/tfjs-node')
+const nsfw = require('nsfwjs')
+
+const app = express()
+const port = 8089
+
+// check url is valid
+function checkIsUrl (value){
+    let pattern = new RegExp('^(https?:\\/\\/(?:www\\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\\.[^\\s]{2,}|www\\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\\.[^\\s]{2,}|https?:\\/\\/(?:www\\.|(?!www))[a-zA-Z0-9]+\\.[^\\s]{2,}|www\\.[a-zA-Z0-9]+\\.[^\\s]{2,})');
+    return pattern.test(value);
+}
+
+//endpoint JPG, PNG, GIF, BMP
+app.get('/image', (req, res) => {
+    // function classify image (jpg,png)
+    async function nudityImage(url) {
+        const pic = await axios.get(url, {
+            responseType: 'arraybuffer',
+        })
+        const model = await nsfw.load() // To load a local model, nsfw.load('file://./path/to/model/')
+        // Image must be in tf.tensor3d format
+        // you can convert image to tf.tensor3d with tf.node.decodeImage(Uint8Array,channels)
+        const image = await tf.node.decodeImage(pic.data,3)
+        const predictions = await model.classify(image)
+        image.dispose() // Tensor memory must be managed explicitly (it is not sufficient to let a tf.Tensor go out of scope for its memory to be released).
+        // json response
+        res.send(JSON.stringify({error:false,message:'image score',data:predictions}))
+    }
+
+    //validate url
+    if(checkIsUrl(req.query.url)){
+        //load function
+        nudityImage(req.query.url);
+    }
+    else{
+        res.send(JSON.stringify({error:true,message:'Invalid parameter',data:[]}));
+    }
+})
+
+//endpoint GIF Animated
+app.get('/imagegif', (req, res) => {
+
+    // function classify image only GIF
+    async function nudityImageGIF(url) {
+        const pic = await axios.get(url, {
+            responseType: 'arraybuffer',
+        })
+        const model = await nsfw.load() // To load a local model, nsfw.load('file://./path/to/model/')
+        const predictions = await model.classifyGif(pic.data)
+        //image.dispose() // Tensor memory must be managed explicitly (it is not sufficient to let a tf.Tensor go out of scope for its memory to be released).
+        // json response
+        res.send(JSON.stringify({error:false,message:'image score',data:predictions}))
+    }
+
+    //validate url
+    if(checkIsUrl(req.query.url)){
+        //load function
+        nudityImageGIF(req.query.url);
+    }
+    else{
+        res.send(JSON.stringify({error:true,message:'Invalid parameter',data:[]}));
+    }
+})
+
+app.listen(port, () => {
+    console.log(`App listening at http://localhost:${port}`)
+})
